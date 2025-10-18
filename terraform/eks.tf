601a810cd5fc0c4cb160795fa635192a681909f1
dineshbalakar <dnshkmr.bal@gmail.com>
2025-10-18 13:53:17 +0100

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,38 +2,44 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
 
-  cluster_name                   = local.name
+  cluster_name                    = local.name
   cluster_endpoint_public_access = true
-
-  cluster_addons = {
-  coredns = {
-    addon_version = "v1.10.1-eksbuild.2"
-  }
-  kube-proxy = {
-    addon_version = "v1.27.1-eksbuild.2"
-  }
-  vpc-cni = {
-    addon_version = "v1.14.1-eksbuild.2"
-  }
-}
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
+  cluster_addons = {
+    coredns = {
+      addon_version            = "v1.10.1-eksbuild.2"
+      resolve_conflicts        = "OVERWRITE"
+      service_account_role_arn = null
+    }
 
-    attach_cluster_primary_security_group = true
+    kube-proxy = {
+      addon_version            = "v1.27.1-eksbuild.2"
+      resolve_conflicts        = "OVERWRITE"
+      service_account_role_arn = null
+    }
+
+    vpc-cni = {
+      addon_version            = "v1.14.1-eksbuild.2"
+      resolve_conflicts        = "OVERWRITE"
+      service_account_role_arn = null
+    }
+  }
+
+  eks_managed_node_group_defaults = {
+    ami_type                               = "AL2_x86_64"
+    instance_types                         = ["m5.large"]
+    attach_cluster_primary_security_group  = true
   }
 
   eks_managed_node_groups = {
-      cluster-wg = {
+    cluster-wg = {
+      desired_size = 1
       min_size     = 1
       max_size     = 2
-      desired_size = 1
 
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
